3e09946b6b06d42da02c3b81dd5f66aa3ef64add
Dhanshri Rc <dhanshrirc@gmail.com>
2026-03-10 12:44:58 +0530

    feat: add Contact, FAQ, and Login components; update Navbar with links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,45 @@
 export default function Navbar() {
   return (
     <div className="bg-black border-b">
-    <div className="w-full flex justify-end pr-10 pb-2 text-white text-xs gap- font-semibold">
-        <a className="hover:text-yellow-400 cursor-pointer">Home</a>
-        <a className="hover:text-yellow-400 cursor-pointer">Log In</a>
-        <a className="hover:text-yellow-400 cursor-pointer">Contacts</a>
-        <a className="hover:text-border-white cursor-pointer">FAQs</a>
-        <a className=" cursor-pointer">INSTICC Portal</a>
+      <div className="w-full flex justify-end pr-10 pb-2 text-white text-xs gap-4 font-bold">
+
+        <a
+          href="/"
+          className="cursor-pointer border-b-2 border-transparent hover:border-white"
+        >
+          Home
+        </a>
+
+        <a
+          href="/login.html"
+          className="cursor-pointer border-b-2 border-transparent hover:border-white"
+        >
+          Log In
+        </a>
+
+        <a
+          href="/contacts.html"
+          className="cursor-pointer border-b-2 border-transparent hover:border-white"
+        >
+          Contacts
+        </a>
+
+        <a
+          href="/faqs.html"
+          className="cursor-pointer border-b-2 border-transparent hover:border-white"
+        >
+          FAQs
+        </a>
+
+        <a
+          href="https://www.insticc.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer border-b-2 border-transparent hover:border-white"
+        >
+          INSTICC Portal
+        </a>
+
       </div>
     </div>
   );
