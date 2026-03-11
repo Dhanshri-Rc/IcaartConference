f28f096c039a5403de1e26a506bab69a8c7e6ca8
Dhanshri Rc <dhanshrirc@gmail.com>
2026-03-11 14:44:24 +0530

    style: adjust padding and text size in Navbar component for improved layout

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <div className="bg-black border-b ">
-      <div className="w-full flex justify-end pr-8 pb-2 text-white text-sm gap-4 font-bold">
+      <div className="w-full flex justify-end pr-4 text-white text-[13px] gap-4 font-bold">
 
         <Link to="/" className="border-b-2 border-transparent hover:border-white">
           Home
