92a73c47dbd339f95c4f1745a491d45792bc4ae7
Dhanshri Rc <dhanshrirc@gmail.com>
2026-03-11 15:17:34 +0530

    style: enhance layout with padding adjustments in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import FAQ from "./pages/FAQ";
 
 function App() {
   return (
-    <div className="bg-black min-h-screen flex justify-center">
-      <div className="w-[980px] bg-white">
+    <div className="bg-black min-h-screen flex justify-center pb-4">
+      <div className="w-[980px] bg-white rounded-b-xl overflow-hidden">
+        
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
@@ -19,7 +20,7 @@ function App() {
         </Routes>
 
         <hr className="my-2 border-dashed border-gray-400 " />
-        <p className="text-[12px] text-black text-end px-2 py-1">
+        <p className="text-[12px] text-black text-end px-4 pb-2">
           © 2026 <span className="text-[#a05000]">INSTICC</span>{" "}
         </p>
       </div>
